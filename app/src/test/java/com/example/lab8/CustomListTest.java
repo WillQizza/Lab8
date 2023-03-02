f4010c8a3a2bc99074f5bb4bc1b23126bca3a433
WillQi <williamqipizza@gmail.com>
2023-03-01 18:12:31 -0700

    Write addCityTest

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,5 +12,27 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    /**
+     * Retrieves the size of the list
+     * Increases the list by adding a new city
+     * Checks if the current size matches the initial size + 1
+     */
+    @Test
+    public void addCityTest() {
+        CustomList list = getCustomList();
+        int initialCount = list.getCount();
+
+        list.addCity(getCity());
+
+        assertEquals(initialCount + 1, list.getCount());
+    }
+
+    private static CustomList getCustomList() {
+        return new CustomList(null, new ArrayList<>());
+    }
+
+    private static City getCity() {
+        return new City("PizzaLand", "The Pizza Province");
+    }
 
 }
